4e14bf8e653e74cff3e2d89bb07737d80bbcf077
RaphaelDias1206 <121405183+RaphaelDias1206@users.noreply.github.com>
2023-09-01 00:46:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+const dynamicText = document.querySelector("h2 span");
+const words = ["Economia", "Investimento", "Educação", "Acessibilidade", "Inovação"];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typeEffect = () => {
+   const currentWord = words[wordIndex];
+   const currentChar = currentWord.substring(0, charIndex);
+   dynamicText.textContent = currentChar;
+   dynamicText.classList.add("stop-blinking")
+
+   if(!isDeleting && charIndex < currentWord.length){
+      charIndex++;
+      setTimeout(typeEffect, 200);
+   } else if(isDeleting && charIndex > 0){
+      charIndex--;
+      setTimeout(typeEffect, 100);
+   }
+   else{
+      isDeleting = !isDeleting;
+      dynamicText.classList.remove("stop-blinking");
+      wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+      setTimeout(typeEffect, 1200);
+   }
+}
+typeEffect();
+
+
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+   hamburger.classList.remove("active");
+   navMenu.classList.remove("active");
+}))
+
+
+
+// search-box open close
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+
+searchBox.addEventListener("click", ()=>{
+  navbar.classList.toggle("showInput");
+  if(navbar.classList.contains("showInput")){
+    searchBox.classList.replace("bx-search" ,"bx-x");
+  }else {
+    searchBox.classList.replace("bx-x" ,"bx-search");
+  }
+});
+
+// sidebar open close
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+navLinks.style.left = "-100%";
+}
+
+// sidebar submenu open close
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function() {
+ navLinks.classList.toggle("show1");
+}
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function() {
+ navLinks.classList.toggle("show2");
+}
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function() {
+ navLinks.classList.toggle("show3");
+}
+
+
+
+window.addEventListener("scroll", function(){
+   var nav = this.document.querySelector("nav")
+   nav.classList.toggle("sticky", this.window.scrollY > 0);
+})
+
+
+
+let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener('change', function() {
+    checkboxes.forEach(otherCheckbox => {
+      if (otherCheckbox !== this) {
+        otherCheckbox.checked = false;
+      }
+    });
+  });
+});
+
+
